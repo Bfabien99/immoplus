9bc6703f03679a713601af7dc95a72b8f04cd7f5
Bfabien99 <fabienbrou99@gmail.com>
2022-09-05 21:59:47 +0000

    api folder initialize

---
+++ api/v1/controller/user.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('db.php');
+require_once('../model/User.php');
+require_once('../model/Response.php');
+
+try {
+    $connectDB = DB::connectDB();
+} catch (PDOException $ex) {
+    error_log("Connection error -" . $ex, 0);
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage("Database connection error");
+    $response->send();
+    exit();
+}
---
+++ api/v1/model/Property.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class PropertyException extends Exception
+{
+}
+
+class Property
+{
+    private $_id;
+
+
+    public function __construct($id){
+        $this->setId($id);
+    }
+
+    public function setID($id)
+    {
+        if (($id !== null) && (!is_numeric($id) || $id <= 0 || $id > 9223372036854775807 || $this->_id !== null)) {
+            throw new PropertyException("Property ID error");
+        }
+        $this->_id = $id;
+    }
+
+    public function getID()
+    {
+        return $this->_id;
+    }
+}
