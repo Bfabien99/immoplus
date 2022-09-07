299bc871b3c14c85ecdfcf9720d21e158f2a0d13
Bfabien99 <fabienbrou99@gmail.com>
2022-09-07 18:57:37 +0000

    get user and property by id

--- api/v1/controller/property.php
+++ api/v1/controller/property.php
@@ -61,4 +61,48 @@ if (empty($_GET)) {
     }
 } elseif (array_key_exists("property_id", $_GET)) {
     $property_id = $_GET["property_id"];
+
+    if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+        try {
+            $sql = "select * from property where id = :id";
+            $query = $connectDB->prepare($sql);
+            $query->bindValue(':id',$property_id, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $propertyArray = [];
+
+            if ($rowCount === 0) {
+                $response = new Response(404, false, "Property not found");
+                $response->send();
+                exit();
+            }
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $property = new Property($row['id']);
+                $propertyArray[] = $property->returnPropertyAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['properties'] = $propertyArray;
+
+            $response = new Response(200, true, "", $returnData);
+            $response->send();
+            exit();
+        } catch (PropertyException $ex) {
+            $response = new Response(400, false, $ex->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $ex) {
+            error_log("Database query error -" . $ex, 0);
+            $response = new Response(500, false, "Failed to get task");
+            $response->send();
+            exit();
+        }
+    }if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+
+    }if ($_SERVER['REQUEST_METHOD'] == 'PATCH'){
+
+    }
 }
--- api/v1/controller/user.php
+++ api/v1/controller/user.php
@@ -61,4 +61,49 @@ if (empty($_GET)) {
     }
 } elseif (array_key_exists("user_id", $_GET)) {
     $user_id = $_GET["user_id"];
+
+    if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+        try {
+            $sql = "select * from user where id = :id";
+            $query = $connectDB->prepare($sql);
+            $query->bindValue(':id',$user_id, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $userArray = [];
+
+            if ($rowCount === 0) {
+                $response = new Response(404, false, "user not found");
+                $response->send();
+                exit();
+            }
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $user = new User($row['id']);
+                $userArray[] = $user->returnUserAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['users'] = $userArray;
+
+            $response = new Response(200, true, "", $returnData);
+            $response->send();
+            exit();
+        } catch (UserException $ex) {
+            $response = new Response(400, false, $ex->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $ex) {
+            error_log("Database query error -" . $ex, 0);
+            $response = new Response(500, false, "Failed to get user");
+            $response->send();
+            exit();
+        }
+
+    }if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+
+    }if ($_SERVER['REQUEST_METHOD'] == 'PATCH'){
+
+    }
 }
