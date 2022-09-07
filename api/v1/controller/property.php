b2b6f1ae03f53b8cff5c0cc77525c7d4cbc210cc
Bfabien99 <fabienbrou99@gmail.com>
2022-09-07 19:55:56 +0000

    add the created property in db

--- api/v1/controller/property.php
+++ api/v1/controller/property.php
@@ -91,7 +91,51 @@ if (empty($_GET)) {
             $type = $newProperty->getType();
             $post_date = $newProperty->getPostDate();
 
-            var_dump($newProperty);
+            // Insertion de la propriété dans la base de donnée
+            $sql = "insert into property (title, description, type) values(:title, :description, :type)";
+            $query = $connectDB->prepare($sql);
+            $query->bindValue(':title', $title, PDO::PARAM_STR);
+            $query->bindValue(':description', $description, PDO::PARAM_STR);
+            $query->bindValue(':type', $type, PDO::PARAM_STR);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            if ($rowCount === 0) {
+                $response = new Response(500, false, "Failed to add property");
+                $response->send();
+                exit();
+            }
+
+            //On récupère la propriété ajoutée
+            $lastTaskID = $connectDB->lastInsertId();
+
+            $sql = "select * from property where id = :id";
+            $query = $connectDB->prepare($sql);
+            $query->bindValue(':id',$lastTaskID, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $propertyArray = [];
+
+            if ($rowCount === 0) {
+                $response = new Response(500, false, "Failed to retrieve property after added");
+                $response->send();
+                exit();
+            }
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $property = new Property($row['id'], $row['title'], $row['description'], $row['type'], $row['post_date']);
+                $propertyArray[] = $property->returnPropertyAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['properties'] = $propertyArray;
+
+            $response = new Response(201, true, "Property added successfully", $returnData);
+            $response->send();
+            exit();
+
         }catch (PropertyException $ex) {
             $response = new Response(400, false, $ex->getMessage());
             $response->send();
