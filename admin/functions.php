9e0b6fd4dc0b9ea99975fbf1e83bcbbb5af1e1d1
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-10 14:06:26 +0000

    get data from my api

--- admin/functions.php
+++ admin/functions.php
@@ -23,6 +23,27 @@ function callApiImgur($postFields)
     return $response;
 }
 
+function GetDataFromMyApi($url)
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+}
+
+
 function PostToMyApi($data, $url)
 {
     $curl = curl_init();
