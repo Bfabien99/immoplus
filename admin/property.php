2be551453660c85fb752e829e304412d8d4786c3
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-10 14:34:44 +0000

    add style to property.php file

--- admin/property.php
+++ admin/property.php
@@ -24,14 +24,44 @@ if($data){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body{
+            overflow-x: hidden;
+        }
+        .properties{
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            gap: 0.5em;
+            padding: 10px;
+            border-radius: 3px;
+        }
+
+        .property{
+            padding: 5px;
+            box-shadow: 0px 2px 5px #ccc;
+        }
+
+        .property .imgBx{
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            height: 200px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <?php if(!empty($properties)):?>
+            <div class="properties">
             <?php foreach ($properties as $property):?>
             <div class="property">
-                <div class="imgBx">
-                    <img src="<?php echo $property['picture'] ?? "" ?>" alt="">
+                <div class="imgBx" style="background-image:url('<?php echo $property['picture'] ?? 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=1100&q=60'?>')">
                 </div>
                 <div class="contentBx">
                     <h3 class="title"><?php echo $property['title'] ?></h3>
@@ -41,6 +71,7 @@ if($data){
                 </div>
             </div>
             <?php endforeach;?>
+            </div>
         <?php else :?>
             <h4>Aucune propriété pour l'instant</h4>
         <?php endif; ?>
