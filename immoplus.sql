951c3a2451574eea66939eb79c9cf5863505b5ce
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-13 14:57:40 +0000

    database update

--- immoplus.sql
+++ immoplus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : jeu. 08 sep. 2022 à 14:23
--- Version du serveur : 10.4.24-MariaDB
--- Version de PHP : 8.1.6
+-- Hôte : localhost
+-- Généré le : mar. 13 sep. 2022 à 16:56
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,20 +45,28 @@ CREATE TABLE `property` (
   `address` text DEFAULT NULL,
   `area` varchar(255) DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
-  `post_date` datetime DEFAULT current_timestamp()
+  `shower` int(11) DEFAULT NULL,
+  `bedroom` int(11) DEFAULT NULL,
+  `picture` text DEFAULT NULL,
+  `post_date` datetime DEFAULT current_timestamp(),
+  `etat` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `property`
 --
 
-INSERT INTO `property` (`id`, `title`, `description`, `type`, `address`, `area`, `price`, `post_date`) VALUES
-(1, 'Property 1', 'A beautiful property', 'location', 'Abidjan', '123', '120000', '2022-09-07 19:09:12'),
-(2, 'Property 2', 'A wonderful Property', 'location', 'Gagnoa', '300', '200000', '2022-09-07 19:09:12'),
-(4, 'Property 3', 'A wonderful Property', 'vendre', 'Cocody', '500', '723000', '2022-09-07 20:12:41'),
-(5, 'Property 4', 'A wonderful Property', 'vendre', 'Angré', '250', '140000', '2022-09-07 20:20:53'),
-(6, 'Property 5', 'A wonderful Property', 'location', 'Abobo', '123', '123000', '2022-09-07 20:59:58'),
-(7, 'Property 6', 'A wonderful Property', 'location', 'Gagnoa', '300', '200000', '2022-09-07 21:58:47');
+INSERT INTO `property` (`id`, `title`, `description`, `type`, `address`, `area`, `price`, `shower`, `bedroom`, `picture`, `post_date`, `etat`) VALUES
+(1, 'Property 1', 'A beautiful property', 'location', 'Abidjan', '123', '120000', NULL, NULL, NULL, '2022-09-07 19:09:12', 0),
+(2, 'Property 2', 'A wonderful Property', 'location', 'Gagnoa', '300', '200000', NULL, NULL, NULL, '2022-09-07 19:09:12', 0),
+(4, 'Property 3', 'A wonderful Property', 'vendre', 'Cocody', '500', '723000', NULL, NULL, NULL, '2022-09-07 20:12:41', 0),
+(5, 'Property 4', 'A wonderful Property', 'vendre', 'Angré', '250', '140000', NULL, NULL, NULL, '2022-09-07 20:20:53', 0),
+(6, 'Property 5', 'A wonderful Property', 'location', 'Abobo', '123', '123000', NULL, NULL, NULL, '2022-09-07 20:59:58', 0),
+(7, 'Property 6', 'A wonderful Property', 'location', 'Gagnoa', '300', '200000', NULL, NULL, NULL, '2022-09-07 21:58:47', 0),
+(8, 'Property 7', 'A wonderful Property', 'location', 'Gagnoa', '300', '200000', 2, 2, 'https://www.unsplash.com/photos/eWqOgJ-lfiI', '2022-09-10 09:37:52', 0),
+(9, 'Property 8', 'A wonderful Property', 'location', 'Agboville', '300', '200000', 2, 2, 'https://www.unsplash.com/photos/eWqOgJ-lfiI', '2022-09-10 09:41:10', 0),
+(10, 'Corporate Intranet Manager', 'Sint perspiciatis cumque a beatae ducimus. Magnam consequatur quasi similique quisquam nisi quasi non magnam. Quia quibusdam ex quia.', 'location', '7156 Walsh Branch', '505', '5026', 137, 559, 'https://i.imgur.com/wfCe9Nz.jpg', '2022-09-10 11:03:15', 0),
+(11, 'Corporate Identity Assistant', 'Ut dicta quis explicabo et perferendis et. Illum non totam dolore eius sit ut in fugit. Adipisci quae vel error ut dicta in facilis.', 'location', '78103 Florence Key', '440', '5548', 442, 47, 'https://i.imgur.com/b7TUcGw.jpg', '2022-09-10 11:25:19', 0);
 
 -- --------------------------------------------------------
 
@@ -106,7 +114,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `property`
 --
 ALTER TABLE `property`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'property_ID', AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'property_ID', AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `users`
