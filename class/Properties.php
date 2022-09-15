99d214ffe9740e746079fcad627df3861c916b45
bfabien99 <fabienbrou99@gmail.com>
2022-09-15 18:18:37 +0000

    add user and new property class

---
+++ class/Properties.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Properties{
+
+        public function getRecent_properties(){
+
+        }
+
+        public function getMostView_properties(){
+
+        }
+
+        public function publishProperties(){
+
+        }
+        
+    }
---
+++ class/Users.php
@@ -0,0 +1,32 @@
+<?php
+
+    class Users{
+
+        public function getUsers(){
+
+        }
+
+        public function getUserById($id){
+
+        }
+
+        public function isUser($email, $pseudo){
+            
+        }
+
+        public function usersLogin($identifiant, $password){
+
+        }
+
+        public function insertUsers($fullname, $contact, $email, $pseudo, $password){
+
+        }
+
+        public function updateUsers($fullname, $contact, $email, $pseudo, $password){
+            
+        }
+
+        public function deleteUsers($id){
+
+        }
+    }
