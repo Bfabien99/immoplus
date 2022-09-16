c11b213a1f698b37675f42847d69446870eff179
bfabien99 <fabienbrou99@gmail.com>
2022-09-16 18:39:03 +0000

    recent properties list css suite

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -264,6 +264,21 @@
             color: var(--white);
             border-radius: 6px;
         }
+
+        .details table{
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+        }
+
+        .details table thead td{
+            font-weight: 600;
+        }
+
+        .details .recentOrders table tr{
+            color: var(--black1);
+            border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        }
     </style>
 </head>
 
