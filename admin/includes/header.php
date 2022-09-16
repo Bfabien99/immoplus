e89bc3462177d0b507e7b114c16b72a722d84fc3
bfabien99 <fabienbrou99@gmail.com>
2022-09-16 23:28:13 +0000

    icon link

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/1f88d87af5.js" crossorigin="anonymous"></script>
     <title>Admin</title>
     <style>
         * {
