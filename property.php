dec4ce59e07009c226b6ee01c94e79dcb2df59c5
bfabien99 <fabienbrou99@gmail.com>
2022-09-17 14:14:20 +0000

    import google fonts

--- property.php
+++ property.php
@@ -26,6 +26,7 @@ if($data){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Property</title>
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;600;800;900&family=Rajdhani&family=Roboto:wght@100;300;400;500;900&display=swap');
         * {
             margin: 0;
             padding: 0;
